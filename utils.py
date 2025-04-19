b653ca70c311798c87d38b7104a3bb1a87971820
MichałJaroszczuk <michal.jaroszczuk@pwr.edu.pl>
2025-04-19 14:18:39 +0200

    Added docstrings

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 # przykladowe funkcje utils .py
 def add(a: int, b: int) -> int:
+    """Return the sum of two input variables"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Return the substraction of two input variables"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Return the multiplication of two input variables"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Return the division of two input variables"""
     return a / b
